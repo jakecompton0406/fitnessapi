3054d5a9e26acdc5e2c8a3191e9fa60101ae8700
jakecompton0406 <jakecompton0406@gmail.com>
2026-01-08 22:28:58 -0600

    Initial commit - Spring Boot API

---
+++ src/main/java/com/jake/fitnessapi/FitnessapiApplication.java
@@ -0,0 +1,13 @@
+package com.jake.fitnessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessapiApplication.class, args);
+	}
+
+}
